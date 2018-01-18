8c6d531dc1955f9801e64a33f0739e283d102afc
bnjasim <bnjasim@gmail.com>
2018-01-18 22:11:04 +0530

    Add init_pick for first few training data during active learning

--- activelearn.py
+++ activelearn.py
@@ -8,4 +8,19 @@ def active_pick(unsup_data, acq_fn, num_samples):
     values = [acq_fn(x) for x in unsup_data]
     pos = np.argpartition(values, -num_samples)[-num_samples:]
     return unsup_data[pos]
-    
+
+
+def init_pick(pool_data, pool_labels, num):
+    '''Pick num number of datapoints from the unsupervised data pool
+    Remove them from the pool and return the data.
+    Returns chosen datapoints and the updated pool_data'''
+    if len(pool_data) < num:
+        raise Exception('pool data is too small')
+        
+    indices = np.random.choice(range(len(pool_data)), num, replace=False)
+    datapoints = pool_data[indices]
+    labels = pool_labels[indices]
+    pool_data = np.delete(pool_data, indices)
+    pool_labels = np.delete(pool_labels, indices)
+    print("Picked " + str(num) + " datapoints\nSize of updated unsupervised pool = " + str(len(pool_labels)) + "\n")
+    return datapoints, labels, pool_data, pool_labels
